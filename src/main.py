5bcd8192d1fd7c60a32ea85a376e657cd2aa8f9c
gormel <gremkil@gmail.com>
2024-07-16 12:42:59 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+  server_address = ('', 80)
+  httpd = server_class(server_address, handler_class)
+  try:
+      httpd.serve_forever()
+  except KeyboardInterrupt:
+      httpd.server_close()
+
+run()
