ae7855e9149c7a3c38ccd5b6ab941888f1832e0d
Roman <gremkil@gmail.com>
2024-07-17 02:49:00 +0300

    dbg: handler

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
+from http.server import BaseHTTPRequestHandler
 from http.server import SimpleHTTPRequestHandler
 from http.server import HTTPServer
 
 class HttpGetHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory="./htdocs", **kwargs)
+        
+class HttpGetHandler1(BaseHTTPRequestHandler):
+    """Обработчик с реализованным методом do_GET."""
 
-def run(server_class=HTTPServer, handler_class=HttpGetHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write('<html><head><meta charset="utf-8">'.encode())
+        self.wfile.write('<title>Простой HTTP-сервер.</title></head>'.encode())
+        self.wfile.write('<body>Был получен GET-запрос.</body></html>'.encode())
+
+
+def run(server_class=HTTPServer, handler_class=HttpGetHandler1):
   server_address = ('', 8081)
   httpd = server_class(server_address, handler_class)
   try:
