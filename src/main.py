bafdd954870ca4d1be68447f3d798c82ac1efb49
Roman <gremkil@gmail.com>
2024-07-17 02:43:59 +0300

    fix: port hosting

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ class HttpGetHandler(SimpleHTTPRequestHandler):
         super().__init__(*args, directory="./htdocs", **kwargs)
 
 def run(server_class=HTTPServer, handler_class=HttpGetHandler):
-  server_address = ('', 80)
+  server_address = ('', 8081)
   httpd = server_class(server_address, handler_class)
   try:
       httpd.serve_forever()
