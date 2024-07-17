a7a5ecd349b96a595715814c07cc3e3e542eca0f
Roman <gremkil@gmail.com>
2024-07-17 15:49:24 +0300

    dbg: chmod + routing

--- src/main.py
+++ src/main.py
@@ -4,26 +4,40 @@ from http.server import HTTPServer
 import os
 from io import open
 
-extra = ""
-
-try:
-    with open("./htdocs/index.html", "r") as f:
-        extra = f.read()
-except Exception as e:
-    extra = str(e)
-
 class HttpGetHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory="./htdocs/", **kwargs)
         
 class HttpGetHandler1(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write('<html><head><meta charset="utf-8">'.encode())
-        self.wfile.write('<title>Простой HTTP-сервер.</title></head>'.encode())
-        self.wfile.write('<body>Был получен GET-запрос. {}</body></html>'.format(extra).encode())
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write('<html><head><meta charset="utf-8">'.encode())
+            self.wfile.write('<title>Простой HTTP-сервер.</title></head>'.encode())
+            self.wfile.write('<body><link href="read_file">go</link></body></html>'.encode())
+        elif self.path == '/read_file':
+            extra = ""
+
+            try:
+                with open("./htdocs/index.html", "r") as f:
+                    extra = f.read()
+            except Exception as e:
+                extra = str(e)
+            
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write('<html><head><meta charset="utf-8">'.encode())
+            self.wfile.write('<title>Простой HTTP-сервер.</title></head>'.encode())
+            self.wfile.write('<body>{}</body></html>'.format(extra).encode())
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'Page not found')
+
 
 
 def run(server_class=HTTPServer, handler_class=HttpGetHandler):
