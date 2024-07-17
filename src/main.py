8c083256a551a76d09b58b456068adf1aef2567a
Roman <gremkil@gmail.com>
2024-07-17 12:40:58 +0300

    dbg: чтение файла

--- src/main.py
+++ src/main.py
@@ -5,27 +5,31 @@ import os
 
 class HttpGetHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory="./htdocs", **kwargs)
+        super().__init__(*args, directory="./htdocs/", **kwargs)
         
 class HttpGetHandler1(BaseHTTPRequestHandler):
     """Обработчик с реализованным методом do_GET."""
 
     def do_GET(self):
-        paths = list()
-        for root, dirs, files in os.walk("."):
-            for file_name in files:
-                file_path = os.path.join(root, file_name)
-                paths.append(file_path)
+        extra = ""
+
+        try:
+            with open("./htdocs/index.html") as f:
+                extra = f.read()
+                pass
+        except any as e:
+            extra = str(e)
+            pass
 
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write('<html><head><meta charset="utf-8">'.encode())
         self.wfile.write('<title>Простой HTTP-сервер.</title></head>'.encode())
-        self.wfile.write('<body>Был получен GET-запрос. {}</body></html>'.format('\n'.join(paths)).encode())
+        self.wfile.write('<body>Был получен GET-запрос. {}</body></html>'.format(extra).encode())
 
 
-def run(server_class=HTTPServer, handler_class=HttpGetHandler1):
+def run(server_class=HTTPServer, handler_class=HttpGetHandler):
   server_address = ('', 8081)
   httpd = server_class(server_address, handler_class)
   try:
