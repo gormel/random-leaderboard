97aa0cebcc59ac08c30178fa75cc4c6a1ee34b05
Roman <gremkil@gmail.com>
2024-07-17 10:42:42 +0300

    dbg: структура папок

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler
 from http.server import SimpleHTTPRequestHandler
 from http.server import HTTPServer
+import os
 
 class HttpGetHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
@@ -10,12 +11,18 @@ class HttpGetHandler1(BaseHTTPRequestHandler):
     """Обработчик с реализованным методом do_GET."""
 
     def do_GET(self):
+        paths = list()
+        for root, dirs, files in os.walk(directory_path):
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                paths.append(file_path)
+
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write('<html><head><meta charset="utf-8">'.encode())
         self.wfile.write('<title>Простой HTTP-сервер.</title></head>'.encode())
-        self.wfile.write('<body>Был получен GET-запрос.</body></html>'.encode())
+        self.wfile.write('<body>Был получен GET-запрос. {}</body></html>'.format('\n'.join(paths)).encode())
 
 
 def run(server_class=HTTPServer, handler_class=HttpGetHandler1):
