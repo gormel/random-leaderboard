ff31d8d08bb0548fd787e5c7bfa40cd6a02ae541
Roman <gremkil@gmail.com>
2024-07-17 16:28:36 +0300

    fix: index.html routing

--- src/main.py
+++ src/main.py
@@ -4,34 +4,17 @@ from http.server import HTTPServer
 import os
 from io import open
 
-class HttpGetHandler(SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory="./htdocs/", **kwargs)
-        
-class HttpGetHandler1(BaseHTTPRequestHandler):
+class Router:
+    pass
+
+class HttpGetHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':                
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
-            with open("./htdocs/index.html", "r") as f:
-                extra = f.read()
-                self.wfile.write(f.read().encode())
-        elif self.path == '/read_file':
-            extra = ""
-
-            try:
-                with open("./htdocs/index.html", "r") as f:
-                    extra = f.read()
-            except Exception as e:
-                extra = str(e)
-            
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write('<html><head><meta charset="utf-8">'.encode())
-            self.wfile.write('<title>Простой HTTP-сервер.</title></head>'.encode())
-            self.wfile.write('<body>{}</body></html>'.format(extra).encode())
+            with open("./htdocs/index.html", "rb") as f:
+                self.wfile.write(f.read())
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/html')
@@ -40,7 +23,7 @@ class HttpGetHandler1(BaseHTTPRequestHandler):
 
 
 
-def run(server_class=HTTPServer, handler_class=HttpGetHandler1):
+def run(server_class=HTTPServer, handler_class=HttpGetHandler):
   server_address = ('', 8081)
   httpd = server_class(server_address, handler_class)
   try:
