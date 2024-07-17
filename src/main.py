3fd4eb82f536f23c77f446733169923b82844113
Roman <gremkil@gmail.com>
2024-07-17 10:47:49 +0300

    fix: просмотр файлов

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ class HttpGetHandler1(BaseHTTPRequestHandler):
 
     def do_GET(self):
         paths = list()
-        for root, dirs, files in os.walk(directory_path):
+        for root, dirs, files in os.walk("."):
             for file_name in files:
                 file_path = os.path.join(root, file_name)
                 paths.append(file_path)
